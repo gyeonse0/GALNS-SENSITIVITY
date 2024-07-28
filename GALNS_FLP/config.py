ba730ca3df3a2e7d3f9c7405bb112738ae00f163
YeonSeo Park <138553324+gyeonse0@users.noreply.github.com>
2024-07-28 14:06:46 +0900

    Add files via upload

---
+++ GALNS_FLP/config.py
@@ -0,0 +1,4 @@
+vrp_file_path = r'C:\Users\User\OneDrive\바탕 화면\realrealrealreal-main\realrealrealreal-main\GALNS_FLP\data\multi_modal_data.vrp'
+backup_file_path = r'C:\Users\User\OneDrive\바탕 화면\realrealrealreal-main\realrealrealreal-main\GALNS_FLP\data\multi_modal_data_backup.vrp'
+
+folder_name = 'GALNS_FLP'
